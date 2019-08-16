8bc82e369c61d468ac2bf5e2366d8aeafbbe8afa
briefguo <gyj9094@gmail.com>
2019-08-16 11:29:44 +0800

    feat(init): 初始化项目

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 yarn add next react react-dom
 
+# 添加
+yarn add --dev typescript @types/react @types/node
+
 mkdir ./pages && touch ./pages/index.tsx
 
-echo "export default ()=>(<div>Hello NextJS</div>)" >>./pages/index.tsx
+echo "export default ()=>(<div>Hello NextJS</div>)" >> ./pages/index.tsx
 
 npx next dev
